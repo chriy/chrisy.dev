30285b17c74e7c9ecdf7bd049977baa3250408f4
chris <y1ang@outlook.com>
2025-10-25 15:00:08 +0800

    feat(ci): init astro

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+import {defineConfig} from 'astro/config';
+import tailwindcss from '@tailwindcss/vite'
+import react from "@astrojs/react";
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://chrisy.dev',
+    base: '/',
+    output: 'static',
+    srcDir: "./src",
+    publicDir: "./public",
+    server: {
+        port: 3000
+    },
+    plugins: [
+        tailwindcss(),
+        "@tailwindcss/postcss",
+    ],
+    integrations: [react()]
+});
