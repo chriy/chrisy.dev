298e0bbb5b7aca952a2a83c7dad63edfbd8a940e
chriy <y1ang@outlook.com>
2025-10-26 22:45:21 +0800

    chore: format some pages

--- src/site.config.ts
+++ src/site.config.ts
@@ -1,23 +1,17 @@
 export const NavLinks: NavItem[] = [
     { link: '/posts', name: 'Posts' },
-    // { link: '/category', name: 'Category' },
+    { link: '/category', name: 'Category' },
     { link: '/archive', name: 'Archive' },
     { link: '/photos', name: 'Photos' },
-    { link: '/about', name: 'About' },
-
-    // { link: '/posts', name: '文章' },
-    // { link: '/category', name: '分类' },
-    // { link: '/timeline', name: '时间轴' },
-    // { link: '/photos', name: '相册' },
-    // { link: '/about', name: '关于' }
+    { link: '/about', name: 'About' }
 ]
 
 
 export const Site = {
     meta: {
         lang: 'en',
-        title: 'Chrisy · Code & Beyond',
+        title: 'Chrisy – Backend developer, Photographer & Hiker',
         url: "https://www.chrisy.dev/",
-        description: 'Passionate about backend engineering, photography, and hiking.',
+        description: 'Hi, I\'m Chrisy — a backend developer from China who loves capturing moments through photography and exploring nature on foot.',
     }
 }
