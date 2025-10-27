84fb8427695eed06e970ef8d42bad8f308911abe
chris <y1ang@outlook.com>
2025-10-27 22:37:27 +0800

    feat(page): base layout format

--- src/site.config.ts
+++ src/site.config.ts
@@ -10,7 +10,7 @@ export const NavLinks: NavItem[] = [
 export const Site = {
     meta: {
         lang: 'en',
-        title: 'Chrisy – Backend developer, Photographer & Hiker',
+        title: 'Backend developer, Photographer & Hiker',
         url: "https://www.chrisy.dev/",
         description: 'Hi, I\'m Chrisy — a backend developer from China who loves capturing moments through photography and exploring nature on foot.',
     }
