b33db2217902e65375114dde103781a00203edb9
chris <y1ang@outlook.com>
2025-11-24 20:43:28 +0800

    feat(font): custom font

---
+++ src/lib/font.ts
@@ -0,0 +1,13 @@
+import localFont from "next/font/local"
+
+// 中文
+export const NotoSansSC = localFont({
+    src: '../../public/fonts/noto-sans-sc-400-regular.woff2',
+    display: "swap"
+})
+
+// 英文和数字
+export const Nunito = localFont({
+    src: '../../public/fonts/nunito-400-regular.woff2',
+    display: "swap"
+})
