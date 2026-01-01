2797faf6ad9afbd1866deb237a4d41fd55404528
chris <y1ang@outlook.com>
2026-01-01 23:44:38 +0800

    feat(page): fonts variable

--- src/lib/font.ts
+++ src/lib/font.ts
@@ -3,11 +3,13 @@ import localFont from "next/font/local"
 // 中文
 export const NotoSansSC = localFont({
     src: '../../public/fonts/noto-sans-sc-400-regular.woff2',
-    display: "swap"
+    variable: '--font-noto-sans-sc',
+    display: "swap",
 })
 
 // 英文和数字
 export const Nunito = localFont({
     src: '../../public/fonts/nunito-400-regular.woff2',
-    display: "swap"
+    variable: '--font-nunito',
+    display: "swap",
 })
