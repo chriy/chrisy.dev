72000b121e1c55c30d50ff95900516f153111324
chris <y1ang@outlook.com>
2026-02-10 10:04:39 +0800

    feat(page): global fonts

--- src/lib/font.ts
+++ src/lib/font.ts
@@ -1,15 +1,37 @@
 import localFont from "next/font/local"
 
-// 中文
-export const NotoSansSC = localFont({
-    src: '../../public/fonts/noto-sans-sc-400-regular.woff2',
+// chinese
+export const noto = localFont({
+    style: 'normal',
+    src: '../../public/fonts/noto-sans-variable.woff2',
     variable: '--font-noto-sans-sc',
     display: "swap",
 })
 
-// 英文和数字
-export const Nunito = localFont({
-    src: '../../public/fonts/nunito-400-regular.woff2',
-    variable: '--font-nunito',
-    display: "swap",
+// english and number
+export const nunito = localFont({
+    src: [{
+        path: '../../public/fonts/nunito/nunito-normal-variable.woff2',
+        style: 'normal',
+    }, {
+        path: '../../public/fonts/nunito/nunito-italic-variable.woff2',
+        style: 'italic',
+    }],
+    variable: '--font-nunito-en',
+    weight: '100 900',
+    display: "swap"
+})
+
+// mono
+export const maple = localFont({
+    src: [{
+        path: '../../public/fonts/maple-mono/maple-normal-variable.woff2',
+        style: 'normal',
+    }, {
+        path: '../../public/fonts/maple-mono/maple-italic-variable.woff2',
+        style: 'italic',
+    }],
+    variable: '--font-maple-en',
+    weight: '100 900',
+    display: "swap"
 })
